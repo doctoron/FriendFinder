7f4855f25373c436c9c2ebff604a636414718a60
doctoron <ron.antonio@me.com>
2019-03-06 20:08:18 -0500

    package.json

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,48 +9,48 @@ const friends = require("../../data/friends")
 
 // A GET Route to  display a JSON of all possible friends
 // module.exports = (app) => {
-  // app.get('/friends', (req, res)=> {
-  //   res.sendFile(path.join(__dirname, "../data/friends.js"));
-  // });
+// app.get('/friends', (req, res)=> {
+//   res.sendFile(path.join(__dirname, "../data/friends.js"));
+// });
 
-module.exports = (app) => {  
+module.exports = (app) => {
   app.get("/api/friends", (req, res) => {
     res.json(friends)
   });
-};
 
-// API POST Requests
-// Below code handles the form response that a user submits and submits data to the server.
-// The form data (JSON object) is pushed to the appropriate js array.
-module.exports = app => {
-app.post("/api/friends", (req, res) => {
-  let newFriend = req.body;
-  console.log(newFriend);
 
-  let newScore = newFriend.scores;
+  // API POST Requests
+  // Below code handles the form response that a user submits and submits data to the server.
+  // The form data (JSON object) is pushed to the appropriate js array.
 
-  let newScoreTot;
-  let bestFriend = { name: "", photo: "", matchScore: Infinity }
+  app.post("/api/friends", (req, res) => {
+    let newFriend = req.body;
+    console.log(newFriend);
 
-  for (i = 0; i < friends.length; i++) {
-    newScoreTot = 0
-    let currentFriend = friends[i]
+    let newScore = newFriend.scores;
 
-    for (j = 0; j < currentFriend.scores.length; j++) {
-      let currentFriendScore = currentFriend.scores[j];
-      let currentUserScore = newScore[j];
-      //Math.absolute function review
-      newScoreTot += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+    let newScoreTot;
+    let bestFriend = { name: "", photo: "", matchScore: Infinity }
+
+    for (i = 0; i < friends.length; i++) {
+      newScoreTot = 0
+      let currentFriend = friends[i]
+
+      for (j = 0; j < currentFriend.scores.length; j++) {
+        let currentFriendScore = currentFriend.scores[j];
+        let currentUserScore = newScore[j];
+        //Math.absolute function review
+        newScoreTot += Math.abs(parseInt(currentUserScore) - parseInt(currentFriendScore));
+      }
+      // As close to currentUserScore as possible will be the best match
+      if (newScoreTot <= bestFriend.matchScore) {
+        bestFriend.name = currentFriend.name;
+        bestFriend.photo = currentFriend.photo;
+        bestFriend.matchScore = newScoreTot;
+      }
     }
-    // As close to currentUserScore as possible will be the best match
-    if (newScoreTot <= bestFriend.matchScore) {
-      bestFriend.name = currentFriend.name;
-      bestFriend.photo = currentFriend.photo;
-      bestFriend.matchScore = newScoreTot;
-    }
-  }
-  friends.push(newFriend);
-  res.json(bestFriend);
-})
+    friends.push(newFriend);
+    res.json(bestFriend);
+  })
 };
 
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,22 +8,24 @@ var path = require("path");
 // ===============================================================================
 // ROUTING
 // ===============================================================================
+// HTML GET Requests
+// Below code handles when users "visit" a page.
+// In each of the below cases the user is shown an HTML page of content
+// ---------------------------------------------------------------------------
 
 module.exports = function(app) {
-  // HTML GET Requests
-  // Below code handles when users "visit" a page.
-  // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-  // app.get('/friends', (req, res)=> {
-  //   res.sendFile(path.join(__dirname, "../data/friends.js"));
-  // });
-
-  app.get('/survey', (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
-  });
 
   // If no matching route is found default to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+  
+  app.get('/survey', (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+  });
+  
+  //
+  app.get('/friends', (req, res)=> {
+    res.sendFile(path.join(__dirname, "../data/friends.js"));
   });
 };
