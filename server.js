57c1f6312d945b5127ce47ea93fa7a3455b835cd
doctoron <ron.antonio@me.com>
2019-03-07 01:45:56 -0500

    updated port listener

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(logger);
 
 
 // Sets an initial port. We"ll use this later in our listener
-const PORT = process.env.PORT |8080;
+const PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
